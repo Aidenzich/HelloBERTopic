93e98ad501df8b9ba7d379cb361c9cdc42b71fe9
Aidenzich <aidenzich0221@gmail.com>
2022-04-14 15:17:24 +0800

    update doc

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
 # helloBERTopic
-## Install Package
+本專案用來對 [BERTopic](https://github.com/MaartenGr/BERTopic) 進行一些應用，日後會針對[作者文章](https://towardsdatascience.com/topic-modeling-with-bert-779f7db187e6)與[論文](https://arxiv.org/abs/2203.05794)做一些快速的摘要與實驗
+
+## 執行結果
+若成功執行完`main.py` 檔案，會在export資料夾中產生以下檔案：
+```
+bar_fig.html
+topic_fig.html
+tot_fig.html
+```
+
+## 專案結構
+```
+.
+├── .gitignore
+├── README.md
+├── data
+│   ├── data.csv
+│   ├── keys
+│   │   └── test.ipynb
+│   ├── keys.txt
+│   └── stopword.txt
+├── exp.ipynb
+├── main.py
+├── requirements.txt
+└── utils.py
+```
+## 安裝環境
 ```
 pip install -r requirements.txt
 ```
 
-## Run 
+## 執行程式
 ```
 python main.py
+```
+### 參數說明
+```
+Hello BERTopics
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --topic_num TOPIC_NUM
+                        設置要分成幾個topic
+  --keyword_file KEYWORD_FILE
+                        設置讀取keyword檔案名稱
+  --model_name MODEL_NAME
+                        設置HuggingFace的PretrainModel名稱
+  --data_file DATA_FILE
+                        設置資料讀取位置
+  --word_sentence_cache WORD_SENTENCE_CACHE
+                        是否讀取斷詞快取(如果沒有cache會走Default流程)
 ```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from xmlrpc.client import Boolean
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # 關閉TF警告訊息
 import pandas as pd
 from bertopic import BERTopic
@@ -7,58 +8,80 @@ from transformers import AutoModelForTokenClassification
 from utils import EXPORT_PATH, set_up, DATA_PATH
 from halo import Halo
 from termcolor import colored
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser(description="Hello BERTopics")
+parser.add_argument('--topic_num', type=int, default=10, help='設置要分成幾個topic')
+parser.add_argument('--keyword_file', type=str, default="keys.txt", help='設置讀取keyword檔案名稱')
+parser.add_argument('--model_name', type=str, default="ckiplab/bert-base-chinese-ws", help="設置HuggingFace的PretrainModel名稱")
+parser.add_argument('--data_file', type=str, default="example_data.csv", help="設置資料讀取位置")
+parser.add_argument('--word_sentence_cache', type=Boolean, default=False, help="是否讀取斷詞快取")
+
 
 if __name__ == "__main__":
-    
-    
+    args = parser.parse_args()
+        
     set_up()
     
     # 設定 Huging Face Pretrained Model
-    MODEL_NAME = "ckiplab/bert-base-chinese-ws"        
-    top_n_topics = 20
-    
-    # 讀取 CKIP 斷詞模型
-    ws = WS(str(DATA_PATH))
-    
+    MODEL_NAME = args.model_name
+    top_n_topics = args.topic_num
+            
     # 讀取資料
-    df = pd.read_csv(DATA_PATH / "data.csv")
-    df = df[["year", "name", "label", "description"]]
+    df = pd.read_csv(DATA_PATH / args.data_file)     
+    sentence_list = df["description"].tolist()  # 我們取原始資料中的'description'欄位來當作訓練資料    
+    timestamps = df.year.tolist()               # 讀取data.csv檔案中的 year 資料，作為我們的timestamp
     
     # 取出斷詞關鍵字
-    keysfile = DATA_PATH / "keys.txt"
+    keysfile = DATA_PATH / args.keyword_file
     with open(keysfile) as file:
         lines = file.read().splitlines() 
 
-    # 建立使用者字典 (幫助斷詞出關鍵字)
-    keydict = { l: 1 for l in lines}
-    dictionary = construct_dictionary(keydict)
+    ws_cache_path = EXPORT_PATH / 'word_sentence_cache.pkl'
+    if args.word_sentence_cache and ws_cache_path.is_file():
+        spinner = Halo(text='Load tokenized cache...', spinner='dots')
+        spinner.start()
+        print("Loading word sentence cache...")
+        word_sentence_list = pickle.load(open(ws_cache_path, 'rb'))
+        spinner.stop()
     
-    # 我們取原始資料中的'description'欄位來當作訓練資料
-    sentence_list = df["description"].tolist()
-    # 讀取data.csv檔案中的 year 資料，作為我們的timestamp
-    timestamps = df.year.tolist()
+    else:
+        # 讀取 CKIP 斷詞模型
+        ws = WS(str(DATA_PATH))
+        
+        # 建立使用者字典 (幫助斷詞出關鍵字)
+        keydict = { l: 1 for l in lines}
+        dictionary = construct_dictionary(keydict)
+                
+        
+        spinner = Halo(text='Tokenizing with CKIP-Tagger', spinner='dots')
+        spinner.start()
+        
+        # 開始斷詞
+        word_sentence_list = ws(
+            sentence_list,
+            sentence_segmentation = True,
+            segment_delimiter_set = {",", "。", ":", "?", "!", ";"},
+            recommend_dictionary = dictionary # 加入斷詞字典
+        )
+        
+        spinner.stop()
+            
+        pickle.dump(word_sentence_list, open(ws_cache_path, 'wb'))
     
-    spinner = Halo(text='Tokenizing with CKIP-Tagger', spinner='dots')    
-    spinner.start()
-    
-    # 開始斷詞
-    word_sentence_list = ws(
-        sentence_list,
-        sentence_segmentation = True,
-        segment_delimiter_set = {",", "。", ":", "?", "!", ";"},
-        recommend_dictionary = dictionary # 加入斷詞字典
-    )
-    
-    spinner.stop()
+        
     
     # 轉換為BERTopic 可接受格式
     ws = [ " ".join(w) for w in word_sentence_list]
     print(colored(f"BERTopics Input Showcase: \n [ { ws[0] }]", 'blue'))
     
+    spinner = Halo(text='Loading HagingFace Pretrained Model', spinner='dots')
+    spinner.start()
     
     # 讀取 Hugingface Pretrained Model
     model = AutoModelForTokenClassification.from_pretrained(MODEL_NAME)
-    
+    spinner.stop()
     # 建立 BERTopic
     topic_model = BERTopic(
         language="chinese", 
@@ -74,14 +97,14 @@ if __name__ == "__main__":
     # 各 Topic TF-IDF 關鍵字直方圖
     bar_fig = topic_model.visualize_barchart(
         top_n_topics=top_n_topics,
-        width=200,
+        width=230,
     )
     
     # 各 Topic 向量分佈圖
     topic_fig = topic_model.visualize_topics(
         top_n_topics=top_n_topics,
         width=1000, 
-        height=600
+        
     )
     
     # 各 Topic 向量分佈圖
@@ -95,3 +118,4 @@ if __name__ == "__main__":
     bar_fig.write_html(EXPORT_PATH / "bar_fig.html")
     topic_fig.write_html(EXPORT_PATH / "topic_fig.html")
     tot_fig.write_html(EXPORT_PATH / "tot_fig.html")
+    print('Done')
